7a49f7dde6838aa596434538ecfc2f1772b7ebaf
Priyansh Khandelwal <khandelwalpriyansh85@gmail.com>
2021-06-01 12:55:59 +0530

    finished version

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ var btnTranslate = document.querySelector("#select-btn");
 var txtArea = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationurl(text){
+    return url + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("Error Occured" , error);
+}
+
+
 btnTranslate.addEventListener("click" , function clickHandler(){
- outputDiv.innerText = "uiuuiu " + txtArea.value
+    var txtInput = txtArea.value;
+ 
+    fetch(getTranslationurl(txtInput))
+ .then(response => response.json())
+ .then(json => {
+     var translatedText = json.contents.translated;
+     outputDiv.innerText = translatedText;
+ })
+ .catch(errorHandler)
+
 });
